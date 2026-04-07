f45bb9967069bdb37edfad9737d6b9d07ffb7db1
Viktor Dukhovni <openssl-users@dukhovni.org>
2026-04-07 21:46:58 +1000

    Precompute some helper objects in each SSL_CTX
    Instead of repeated fetching, precompute the below per the library
    context and properties of the SSL_CTX and use them for the lifetime of
    the SSL_CTX.
    
        - HMAC algorithm handle (session ticket HMAC)
        - SHA2-256 algorithm handle (session ticket HMAC)
        - AES_256-CBC algorithm handle (session ticket en/decryption)
        - TLS1 PRF (when TLS <= 1.2 is supported)
    
    The "sha1" and "md5" handles are no longer used, and those fields are
    removed.
    
    The `SSL_HMAC` objects used internally are now stack allocated, and the
    associated "new" and "free" functions are now called "construct" and
    "destruct" respectively.
    
    Reviewed-by: Matt Caswell <matt@openssl.foundation>
    Reviewed-by: Nikola Pajkovsky <nikolap@openssl.org>
    MergeDate: Mon Apr 13 09:03:45 2026
    (Merged from https://github.com/openssl/openssl/pull/30696)

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -3088,7 +3088,6 @@ MSG_PROCESS_RETURN tls_process_new_session_ticket(SSL_CONNECTION *s,
     unsigned int sess_len;
     RAW_EXTENSION *exts = NULL;
     PACKET nonce;
-    EVP_MD *sha256 = NULL;
     SSL_CTX *sctx = SSL_CONNECTION_GET_CTX(s);
 
     PACKET_null_init(&nonce);
@@ -3198,25 +3197,16 @@ MSG_PROCESS_RETURN tls_process_new_session_ticket(SSL_CONNECTION *s,
      * We choose the former approach because this fits in with assumptions
      * elsewhere in OpenSSL. The session ID is set to the SHA256 hash of the
      * ticket.
-     */
-    sha256 = EVP_MD_fetch(sctx->libctx, "SHA2-256", sctx->propq);
-    if (sha256 == NULL) {
-        /* Error is already recorded */
-        SSLfatal_alert(s, SSL_AD_INTERNAL_ERROR);
-        goto err;
-    }
-    /*
+     *
      * We use sess_len here because EVP_Digest expects an int
      * but s->session->session_id_length is a size_t
      */
     if (!EVP_Digest(s->session->ext.tick, ticklen,
             s->session->session_id, &sess_len,
-            sha256, NULL)) {
+            sctx->sha256, NULL)) {
         SSLfatal(s, SSL_AD_INTERNAL_ERROR, ERR_R_EVP_LIB);
         goto err;
     }
-    EVP_MD_free(sha256);
-    sha256 = NULL;
     s->session->session_id_length = sess_len;
     s->session->not_resumable = 0;
 
@@ -3255,7 +3245,6 @@ MSG_PROCESS_RETURN tls_process_new_session_ticket(SSL_CONNECTION *s,
 
     return MSG_PROCESS_CONTINUE_READING;
 err:
-    EVP_MD_free(sha256);
     OPENSSL_free(exts);
     return MSG_PROCESS_ERROR;
 }
